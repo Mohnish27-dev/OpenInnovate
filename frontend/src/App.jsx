0522efe2acfc5a54970444648d5218f8253d70ce
NEZ04 <shubhams.dd24.cs@nitp.ac.in>
2025-11-01 17:12:14 +0530

    front end

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import CollabWorks from "./pages/collabWorkspace/collabWorks";
+import LandingPage from "./pages/landingPage/landingPage";
+import Login from "./pages/login/login";
+import Resume from "./pages/resume/resume";
+
+const App = ()=>{
+  return(
+<>
+    {/* <LandingPage/> */}
+    {/* <Resume /> */}
+    <CollabWorks/>
+    </>
+  )
+}
+export default App;
